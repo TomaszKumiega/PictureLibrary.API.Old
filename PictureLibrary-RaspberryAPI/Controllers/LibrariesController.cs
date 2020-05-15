89df04fbcc0a07b3716e1471f4a1f7e345682569
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-05-15 16:30:37 +0200

    Add logger to libraries controller

--- PictureLibrary-RaspberryAPI/Controllers/LibrariesController.cs
+++ PictureLibrary-RaspberryAPI/Controllers/LibrariesController.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using PictureLibraryModel.Services;
 
 namespace PictureLibrary_RaspberryAPI.Controllers
 {
     public class LibrariesController
     {
+        private readonly ILogger<FileSystemService> _logger;
+
+        public LibrariesController()
+        {
+
+        }
     }
 }
