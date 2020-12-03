dc00bedcae63d7e6ded27796d27e21004efb5131
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-12-03 08:34:03 +0100

    Add library entitities factory

---
+++ PictureLibrary-RaspberryAPI/Model/ILibraryEntitiesFactory.cs
@@ -0,0 +1,16 @@
+﻿using PictureLibraryModel.Services;
+using System.Collections.Generic;
+
+namespace PictureLibraryModel.Model
+{
+    public interface ILibraryEntitiesFactory
+    {
+        Album GetAlbum(string name);
+        Album GetAlbum(string name, List<ImageFile> images);
+        ImageFile GetImageFile();
+        ImageFile GetImageFile(string path);
+        Library GetLibrary(IFileSystemService fileSystemService);
+        Library GetLibrary(string fullPath, string name, List<Album> albums, IFileSystemService fileSystemService);
+        Library GetLibrary(string fullPath, string name, IFileSystemService fileSystemService);
+    }
+}
---
+++ PictureLibrary-RaspberryAPI/Model/LibraryEntitiesFactory.cs
@@ -0,0 +1,45 @@
+﻿using PictureLibraryModel.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Model
+{
+    public class LibraryEntitiesFactory : ILibraryEntitiesFactory
+    {
+        public Library GetLibrary(IFileSystemService fileSystemService)
+        {
+            return new Library(fileSystemService);
+        }
+
+        public Library GetLibrary(string fullPath, string name, IFileSystemService fileSystemService)
+        {
+            return new Library(fullPath, name, fileSystemService);
+        }
+
+        public Library GetLibrary(string fullPath, string name, List<Album> albums, IFileSystemService fileSystemService)
+        {
+            return new Library(fullPath, name, albums, fileSystemService);
+        }
+
+        public ImageFile GetImageFile(string path)
+        {
+            return new ImageFile(path);
+        }
+
+        public ImageFile GetImageFile()
+        {
+            return new ImageFile();
+        }
+
+        public Album GetAlbum(string name)
+        {
+            return new Album(name);
+        }
+
+        public Album GetAlbum(string name, List<ImageFile> images)
+        {
+            return new Album(name, images);
+        }
+    }
+}
