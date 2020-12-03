a15a4c258bffdb852586d2236fd63e43e829b95b
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-12-03 08:33:25 +0100

    Add ILibraryEntity interface

---
+++ PictureLibrary-RaspberryAPI/Model/ILibraryEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictureLibraryModel.Model
+{
+    public interface ILibraryEntity
+    {
+        string ImageSource { get; }
+        string Name { get; }
+    }
+}
