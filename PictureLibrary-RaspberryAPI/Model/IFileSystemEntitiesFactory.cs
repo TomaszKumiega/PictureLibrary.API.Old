b293f9de6cfd51678ee6b5a1048a5394db9bd16b
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-12-03 08:39:00 +0100

    Add file system entities factory

---
+++ PictureLibrary-RaspberryAPI/Model/IFileSystemEntitiesFactory.cs
@@ -0,0 +1,15 @@
+﻿using PictureLibraryModel.Services;
+using System.Collections.ObjectModel;
+
+namespace PictureLibraryModel.Model
+{
+    public interface IFileSystemEntitiesFactory
+    {
+        Directory GetDirectory(ObservableCollection<object> children, string name);
+        Directory GetDirectory(string fullPath, ObservableCollection<object> children);
+        Directory GetDirectory(string fullPath, string name, IFileSystemService fileSystemService);
+        Drive GetDrive(string name, IFileSystemService fileSystemService);
+        ImageFile GetImageFile();
+        ImageFile GetImageFile(string path);
+    }
+}
