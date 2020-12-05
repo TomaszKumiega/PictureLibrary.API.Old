44d398a0388a929b558e05a6bd0dfdb755f6ef93
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-12-05 07:05:35 +0100

    Add getlibrary endpoint

--- PictureLibrary-RaspberryAPI/Controllers/LibrariesController.cs
+++ PictureLibrary-RaspberryAPI/Controllers/LibrariesController.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using PictureLibraryModel.Model;
 using PictureLibraryModel.Services;
 
 namespace PictureLibrary_RaspberryAPI.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class LibrariesController
+    public class LibrariesController : ControllerBase
     {
         private readonly ILogger<LibrariesController> _logger;
         private ILibraryFileService _libraryFileService;
@@ -19,6 +20,20 @@ namespace PictureLibrary_RaspberryAPI.Controllers
         {
             _logger = logger;
             _libraryFileService = libraryFileService;
+        }
+
+
+        [HttpGet("{name}")]
+        public async Task<ActionResult<Library>> GetLibrary(string name)
+        {
+            var library = await _libraryFileService.FindLibrary(name);
+
+            if (library == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(library);
         }
     }
 }
