e8853e9c22f1dd687c7501399f969de2f3fc7d17
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-12-05 06:52:28 +0100

    Add constructor

--- PictureLibrary-RaspberryAPI/Controllers/LibrariesController.cs
+++ PictureLibrary-RaspberryAPI/Controllers/LibrariesController.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PictureLibraryModel.Services;
 
 namespace PictureLibrary_RaspberryAPI.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class LibrariesController
     {
-        private readonly ILogger<FileSystemService> _logger;
+        private readonly ILogger<LibrariesController> _logger;
+        private ILibraryFileService _libraryFileService;
 
-        public LibrariesController()
+        public LibrariesController(ILogger<LibrariesController> logger, ILibraryFileService libraryFileService)
         {
-            _logger = new Logger<FileSystemService>(new LoggerFactory());
+            _logger = logger;
+            _libraryFileService = libraryFileService;
         }
     }
 }
