24082fcccdbe201df2b7da6888bae493aa763c39
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-12-08 08:57:34 +0100

    Change library model

--- PictureLibrary-API/Model/Library.cs
+++ PictureLibrary-API/Model/Library.cs
@@ -8,27 +8,32 @@ namespace PictureLibraryModel.Model
 {
     public class Library : ILibraryEntity
     {
-        public string FullPath { get; }
-        public string Name { get; }
-        public List<Tag> Albums { get; }
+        public string FullPath { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Tag> Tags { get; set; }
+        public List<ImageFile> Images { get; set; }
 
         public Library()
         {
-            Albums = new List<Tag>();
+            Tags = new List<Tag>();
+            Images = new List<ImageFile>();
         }
 
         public Library(string fullPath, string name)
         {
             FullPath = fullPath;
             Name = name;
-            Albums = new List<Tag>();
+            Tags = new List<Tag>();
+            Images = new List<ImageFile>();
         }
 
-        public Library(string fullPath, string name, List<Tag> albums)
+        public Library(string fullPath, string name, List<Tag> tags, List<ImageFile> images)
         {
             FullPath = fullPath;
             Name = name;
-            Albums = albums;
+            Tags = tags;
+            Images = images;
         }
     }
 }
