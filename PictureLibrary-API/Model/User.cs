51f9b0308fc7e13cc3eaac60eaa2cbe28eb90958
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-12-08 09:11:15 +0100

    Add user model

---
+++ PictureLibrary-API/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Model
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
