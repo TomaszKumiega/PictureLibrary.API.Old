bd6c669c60f4ebb4613c7957fd4d7ed7ab620746
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-12-29 06:42:43 +0100

    Add repository interface

---
+++ PictureLibrary-API/Repositories/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Repositories
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(Guid id);
+
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+
+        void Remove(Guid id);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+        T Find(System.Predicate<T> predicate);
+
+        void Update(T entity);
+    }
+}
