4f56648f00b746612bfdebf89e16d613421710d6
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-12-30 08:57:10 +0100

    Refactor add method

--- PictureLibrary-API/Repositories/LibraryRepository.cs
+++ PictureLibrary-API/Repositories/LibraryRepository.cs
@@ -24,62 +24,10 @@ namespace PictureLibrary_API.Repositories
 
         public Library Add(Library entity)
         {
-            // create library file
+            
             var fileStream = _fileSystemService.CreateFile(entity.Name, "Libraries/");
 
-            if (fileStream == null) throw new Exception("File creation error");
-
-            // write all owners in one string
-            string owners = "";
-            foreach (var o in entity.Owners) owners += o.ToString() + ',';
-
-            // create library element
-            var libraryElement = new XElement("library", new XAttribute("name", entity.Name),
-                new XAttribute("description", entity.Description), new XAttribute("owners", owners));
-
-            // create tags elements
-            var tagsElement = new XElement("tags");
-
-            foreach(var t in entity.Tags)
-            {
-                var tagElement = new XElement("tag", new XAttribute("name", t.Name), new XAttribute("description", t.Description));
-
-                tagsElement.Add(tagElement);
-            }
-
-            libraryElement.Add(tagsElement);
-
-            // create images elements
-            var imagesElement = new XElement("images");
-
-            foreach(var i in entity.Images)
-            {
-                var imageFileElement = new XElement("imageFile", new XAttribute("name", i.Name), new XAttribute("extension", i.Extension),
-                    new XAttribute("source", i.Source), new XAttribute("creationTime", i.CreationTime.ToString()), new XAttribute("lastAccessTime", i.LastAccessTime.ToString()),
-                    new XAttribute("lastWriteTime", i.LastWriteTime.ToString()), new XAttribute("size", i.Size.ToString()));
-
-                imagesElement.Add(imageFileElement);
-            }
-
-            libraryElement.Add(imagesElement);
-
-            // save elements to the file
-            try
-            {
-                using (var streamWriter = new StreamWriter(fileStream))
-                {
-                    var xmlWriter = new XmlTextWriter(streamWriter);
-
-                    xmlWriter.Formatting = Formatting.Indented;
-                    xmlWriter.Indentation = 4;
-
-                    libraryElement.Save(xmlWriter);
-                }
-            }
-            catch(Exception e)
-            {
-                _logger.LogError(e, e.Message);
-            }
+            WriteLibraryToFileStream(fileStream, entity);
 
             entity.FullPath = fileStream.Name;
             return entity;
@@ -216,6 +164,63 @@ namespace PictureLibrary_API.Repositories
             library.Images = images;
 
             return library;
+        }
+
+        private void WriteLibraryToFileStream(FileStream fileStream, Library entity)
+        {
+            if (fileStream == null) throw new Exception("File creation error");
+
+            // write all owners in one string
+            string owners = "";
+            foreach (var o in entity.Owners) owners += o.ToString() + ',';
+
+            // create library element
+            var libraryElement = new XElement("library", new XAttribute("name", entity.Name),
+                new XAttribute("description", entity.Description), new XAttribute("owners", owners));
+
+            // create tags elements
+            var tagsElement = new XElement("tags");
+
+            foreach (var t in entity.Tags)
+            {
+                var tagElement = new XElement("tag", new XAttribute("name", t.Name), new XAttribute("description", t.Description));
+
+                tagsElement.Add(tagElement);
+            }
+
+            libraryElement.Add(tagsElement);
+
+            // create images elements
+            var imagesElement = new XElement("images");
+
+            foreach (var i in entity.Images)
+            {
+                var imageFileElement = new XElement("imageFile", new XAttribute("name", i.Name), new XAttribute("extension", i.Extension),
+                    new XAttribute("source", i.Source), new XAttribute("creationTime", i.CreationTime.ToString()), new XAttribute("lastAccessTime", i.LastAccessTime.ToString()),
+                    new XAttribute("lastWriteTime", i.LastWriteTime.ToString()), new XAttribute("size", i.Size.ToString()));
+
+                imagesElement.Add(imageFileElement);
+            }
+
+            libraryElement.Add(imagesElement);
+
+            // save elements to the file
+            try
+            {
+                using (var streamWriter = new StreamWriter(fileStream))
+                {
+                    var xmlWriter = new XmlTextWriter(streamWriter);
+
+                    xmlWriter.Formatting = Formatting.Indented;
+                    xmlWriter.Indentation = 4;
+
+                    libraryElement.Save(xmlWriter);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+            }
         }
     }
 }
