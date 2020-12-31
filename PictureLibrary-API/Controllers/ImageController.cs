240bb09ad745b73098603e1b1ef0157c93d3edda
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-12-31 08:33:49 +0100

    Add image controller

---
+++ PictureLibrary-API/Controllers/ImageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PictureLibrary_API.Model;
+using PictureLibrary_API.Repositories;
+
+namespace PictureLibrary_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly ILogger<ImageController> _logger;
+        private IRepository<Dictionary<ImageFile, byte[]>> _imageRepository;
+
+        public ImageController(ILogger<ImageController> logger, IRepository<Dictionary<ImageFile,byte[]>> imageRepository)
+        {
+            _logger = logger;
+            _imageRepository = imageRepository;
+        }
+    }
+}
