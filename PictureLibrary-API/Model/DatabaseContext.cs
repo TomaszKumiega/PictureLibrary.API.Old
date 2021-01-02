8003bfd48fa46fecc1b1c7edbf71be88bbc5cd7a
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-02 15:01:19 +0100

    Add database context

---
+++ PictureLibrary-API/Model/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Model
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- PictureLibrary-API/Model/User.cs
+++ PictureLibrary-API/Model/User.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PictureLibrary_API.Model
 {
+    [Table("Users")]
     public class User
     {
+        [Key()]
         public Guid Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
