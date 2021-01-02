0f57708e861f46ebdf1446572626b43eb76154a9
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-02 15:29:07 +0100

    Add developement connection string and DI config

--- PictureLibrary-API/Startup.cs
+++ PictureLibrary-API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +33,15 @@ namespace PictureLibrary_API
             services.AddTransient<IFileSystemService, FileSystemService>();
             services.AddTransient<ILibraryRepository, LibraryRepository>();
             services.AddTransient<IImageRepository, ImageRepository>();
+
+            services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseSqlServer(Microsoft
+                    .Extensions
+                    .Configuration
+                    .ConfigurationExtensions.GetConnectionString(this.Configuration, "UsersDatabase"));
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
