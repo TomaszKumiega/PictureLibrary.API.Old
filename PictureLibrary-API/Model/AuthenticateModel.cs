bcb72791ed7d0f2343f3fe5a156363f8d35a0c65
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-02 16:25:26 +0100

    Add AuthenticateModel

---
+++ PictureLibrary-API/Model/AuthenticateModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Model
+{
+    public class AuthenticateModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public AuthenticateModel()
+        {
+
+        }
+
+        public AuthenticateModel(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
