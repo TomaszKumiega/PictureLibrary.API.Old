2d714f1d72c4d11aba9a715222d9a68fd6edd827
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-02 16:07:57 +0100

    Add UpdateModel

---
+++ PictureLibrary-API/Model/UpdateModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Model
+{
+    public class UpdateModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Karma { get; set; }
+
+        public UpdateModel()
+        {
+
+        }
+
+        public UpdateModel(string firstName, string lastName, string username, string password, string email, int karma)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Username = username;
+            Password = password;
+            Email = email;
+            Karma = karma;
+        }
+    }
+}
