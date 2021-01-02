55fb2fa9dc42ca2bcb1c38af5799c9441d7f094f
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-02 16:07:24 +0100

    Add RegisterModel

---
+++ PictureLibrary-API/Model/RegisterModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Model
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        public RegisterModel()
+        {
+
+        }
+
+        public RegisterModel(string username, string password, string email, string firstName, string lastName)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            LastName = lastName;
+            FirstName = firstName;
+        }
+    }
+}
