d3ed9dd60026df3b1bad721baf3258991735054a
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-02 16:22:58 +0100

    Add UsersController

---
+++ PictureLibrary-API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using PictureLibrary_API.Helpers;
+using PictureLibrary_API.Services;
+
+namespace PictureLibrary_API.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+        private IUserService _userService;
+        private IMapper _mapper;
+        private readonly AppSettings _appSettings;
+
+        public UsersController(ILogger<UsersController> logger, IMapper mapper, IOptions<AppSettings> appSettings, IUserService userService)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _appSettings = appSettings.Value;
+            _userService = userService;
+        }
+    }
+}
