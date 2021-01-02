93ff6006613d28e4a187413691b47e84985a99e2
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-02 15:48:37 +0100

    Add UserService

---
+++ PictureLibrary-API/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using PictureLibrary_API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+        User GetById(Guid id);
+        User Create(User user, string password);
+        void Update(User user, string password = null);
+        void Delete(Guid id);
+    }
+}
---
+++ PictureLibrary-API/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using PictureLibrary_API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Services
+{
+    public class UserService : IUserService
+    {
+        public User Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Create(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User user, string password = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
