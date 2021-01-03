b42b206bf8fc99c3595a22d5016832b8e432ca2e
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-03 06:57:36 +0100

    Add UserPresentationModel

---
+++ PictureLibrary-API/Model/UserPresentationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Model
+{
+    public class UserPresentationModel
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+    }
+}
