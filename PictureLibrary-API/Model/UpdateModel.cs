d644ec55150e851dc21217907b8259522dd00767
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-05 10:37:57 +0100

    Change UpdateModel

--- PictureLibrary-API/Model/UpdateModel.cs
+++ PictureLibrary-API/Model/UpdateModel.cs
@@ -7,26 +7,20 @@ namespace PictureLibrary_API.Model
 {
     public class UpdateModel
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public int Karma { get; set; }
 
         public UpdateModel()
         {
 
         }
 
-        public UpdateModel(string firstName, string lastName, string username, string password, string email, int karma)
+        public UpdateModel(string username, string password, string email)
         {
-            FirstName = firstName;
-            LastName = lastName;
             Username = username;
             Password = password;
             Email = email;
-            Karma = karma;
         }
     }
 }
