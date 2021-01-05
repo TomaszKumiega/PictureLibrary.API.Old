25cff27be20b8eda4a297b95a35cc18fa68363ff
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-01-05 09:37:09 +0100

    Add database context

--- PictureLibrary-API/Services/RefreshTokenService.cs
+++ PictureLibrary-API/Services/RefreshTokenService.cs
@@ -8,6 +8,13 @@ namespace PictureLibrary_API.Services
 {
     public class RefreshTokenService : IRefreshTokenService
     {
+        private DatabaseContext _context;
+
+        public RefreshTokenService(DatabaseContext context)
+        {
+            _context = context;
+        }
+
         public void DeleteRefreshToken(string userId, string refreshToken)
         {
             throw new NotImplementedException();
