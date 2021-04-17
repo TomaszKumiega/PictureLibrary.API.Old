e899b5f95e4723e5dadf813860f5e82e823cdc74
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-17 21:49:44 +0200

    Add ImageFileBuilder

---
+++ PictureLibrary-API/Model/Builders/ImageFileBuilder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Model.Builders
+{
+    public class ImageFileBuilder : IImageFileBuilder
+    {
+        private ImageFile ImageFile { get; set; }
+
+        public ImageFile Build()
+        {
+            return ImageFile;
+        }
+
+        public IImageFileBuilder StartBuilding()
+        {
+            ImageFile = new ImageFile();
+            return this;
+        }
+
+        public IImageFileBuilder WithCreationTime(DateTime creationTime)
+        {
+            ImageFile.CreationTime = creationTime;
+            return this;
+        }
+
+        public IImageFileBuilder WithExtension(ImageExtension extension)
+        {
+            ImageFile.Extension = extension;
+            return this;
+        }
+
+        public IImageFileBuilder WithExtension(string extension)
+        {
+            ImageFile.Extension = ImageExtensionHelper.GetExtension(extension);
+            return this;
+        }
+
+        public IImageFileBuilder WithFullName(string fullName)
+        {
+            ImageFile.FullPath = fullName;
+            return this;
+        }
+
+        public IImageFileBuilder WithLastAccessTime(DateTime lastAccessTime)
+        {
+            ImageFile.LastAccessTime = lastAccessTime;
+            return this;
+        }
+
+        public IImageFileBuilder WithLastWriteTime(DateTime lastWriteTime)
+        {
+            ImageFile.LastWriteTime = lastWriteTime;
+            return this;
+        }
+
+        public IImageFileBuilder WithLibraryFullName(string libraryFullName)
+        {
+            ImageFile.LibraryFullPath = libraryFullName;
+            return this;
+        }
+
+        public IImageFileBuilder WithName(string name)
+        {
+            ImageFile.Name = name;
+            return this;
+        }
+
+        public IImageFileBuilder WithSize(long bytes)
+        {
+            ImageFile.Size = bytes;
+            return this;
+        }
+
+        public IImageFileBuilder WithTags(IEnumerable<Tag> tags)
+        {
+            ImageFile.Tags = tags.ToList();
+            return this;
+        }
+    }
+}
