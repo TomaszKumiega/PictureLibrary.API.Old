0801cec18c7674eaef2c7204d9a6f430630e6904
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-17 23:28:17 +0200

    Add ContentNotFoundException

---
+++ PictureLibrary-API/Exceptions/ContentNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Exceptions
+{
+    public class ContentNotFoundException : Exception
+    {
+        public ContentNotFoundException() : base()
+        {
+
+        }
+
+        public ContentNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public ContentNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
