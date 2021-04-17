b897a1701c071f6544ce59bb2d946a9c6751eb26
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-17 23:14:55 +0200

    Add UserAlreadyExistsException

---
+++ PictureLibrary-API/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PictureLibrary_API.Exceptions
+{
+    public class UserAlreadyExistsException : Exception
+    {
+        public UserAlreadyExistsException(string message) : base(message)
+        {
+        }
+
+        public UserAlreadyExistsException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
