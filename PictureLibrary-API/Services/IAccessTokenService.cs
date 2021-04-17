9de9b526a444f84c0517af7654ace513f7dfb356
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-17 20:09:14 +0200

    Add comments

--- PictureLibrary-API/Services/IAccessTokenService.cs
+++ PictureLibrary-API/Services/IAccessTokenService.cs
@@ -9,11 +9,40 @@ namespace PictureLibrary_API.Services
 {
     public interface IAccessTokenService
     {
+        /// <summary>
+        /// Generates new refresh token.
+        /// </summary>
+        /// <returns></returns>
         string GenerateRefreshToken();
+        /// <summary>
+        /// Retrieves refresh token from database.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         string GetRefreshToken(string userId);
+        /// <summary>
+        /// Saves refresh token in database.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="refreshToken"></param>
         void SaveRefreshToken(string userId, string refreshToken);
+        /// <summary>
+        /// Removes refresh token from database.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="refreshToken"></param>
         void DeleteRefreshToken(string userId, string refreshToken);
+        /// <summary>
+        /// Generates new access token.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         string GenerateAccessToken(string userId);
+        /// <summary>
+        /// Retrieves principal from expired token.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
     }
 }
