1ce6c9a941de8bc91b29eca2de1bb1f914348159
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-17 22:32:59 +0200

    Add CreateDirectory method

--- PictureLibrary-API/Services/DirectoryService.cs
+++ PictureLibrary-API/Services/DirectoryService.cs
@@ -8,6 +8,11 @@ namespace PictureLibrary_API.Services
 {
     public class DirectoryService : IDirectoryService
     {
+        public void CreateDirectory(string path)
+        {
+            Directory.CreateDirectory(path);
+        }
+
         public List<string> FindFiles(string directory, string searchPattern)
         {
             var files = Directory.GetFiles(directory, searchPattern, System.IO.SearchOption.AllDirectories).ToList();
--- PictureLibrary-API/Services/IDirectoryService.cs
+++ PictureLibrary-API/Services/IDirectoryService.cs
@@ -9,11 +9,17 @@ namespace PictureLibrary_API.Services
     public interface IDirectoryService
     {
         /// <summary>
-        /// Finds all files in a specified directory matching the search pattern
+        /// Finds all files in a specified directory matching the search pattern.
         /// </summary>
         /// <param name="searchPattern"></param>
         /// <param name="directory"></param>
         /// <returns></returns>
         List<string> FindFiles(string directory, string searchPattern);
+        
+        /// <summary>
+        /// Creates all directories and subdirectories at specified path unless they already exist.
+        /// </summary>
+        /// <param name="path"></param>
+        void CreateDirectory(string path);
     }
 }
