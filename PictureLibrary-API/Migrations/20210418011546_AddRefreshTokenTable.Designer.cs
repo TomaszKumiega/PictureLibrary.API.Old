d246409f5bf1182ff774eb214a2d9c135382943c
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-18 03:17:10 +0200

    Add new migration

--- PictureLibrary-API/Migrations/20210418011546_AddRefreshTokenTable.Designer.cs
+++ PictureLibrary-API/Migrations/20210418011546_AddRefreshTokenTable.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PictureLibrary_API.Model;
 
 namespace PictureLibrary_API.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210418011546_AddRefreshTokenTable")]
+    partial class AddRefreshTokenTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
