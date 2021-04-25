25f7613620aa52c496c68ccb6d7bb7db39aaa3d3
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-25 15:47:36 +0200

    Remove unnecessary where method

--- PictureLibrary-API/Services/UserService.cs
+++ PictureLibrary-API/Services/UserService.cs
@@ -68,7 +68,7 @@ namespace PictureLibrary_API.Services
 
         public void Delete(Guid id)
         {
-            var user = DatabaseContext.Users.Where(x => x.Id == id).FirstOrDefault();
+            var user = DatabaseContext.Users.FirstOrDefault(x => x.Id == id);
             if (user != null)
             {
                 DatabaseContext.Users.Remove(user);
