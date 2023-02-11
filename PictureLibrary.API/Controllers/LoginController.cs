46f32a846d8d602fee64e4eb0896f91f879e3c69
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-02-11 16:25:20 +0100

    Add login controller

---
+++ PictureLibrary.API/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PictureLibrary.Model;
+
+namespace PictureLibrary.API.Controllers
+{
+    [Route("login")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = new User(); // download user from database
+
+            if (user != null)
+            {
+                string token = "token"; // generate token
+                return Ok(token);
+            }
+
+            return NotFound("User not found");
+        }
+    }
+}
