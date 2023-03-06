98e753449ae6101c7e47cd4c973a31d5a6a25db9
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-03-06 17:53:31 +0100

    Add models

---
+++ PictureLibrary.Model/Image/ImageFile.cs
@@ -0,0 +1,12 @@
+﻿namespace PictureLibrary.Model
+{
+    public class ImageFile
+    {
+        public required string Name { get; set; }
+        public required string Extension { get; set; }
+        public required long Size { get; set; }
+        public required IEnumerable<Library> Libraries { get; set; }
+        public required IEnumerable<User> Owners { get; set; }
+        public IEnumerable<Tag>? Tags { get; set; }
+    }
+}
---
+++ PictureLibrary.Model/Tag/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace PictureLibrary.Model
+{
+    public class Tag
+    {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public required string ColorHex { get; set; }
+        public required IEnumerable<Library> Libraries { get; set; }
+    }
+}
