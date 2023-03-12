a682b81447be927e28bebb131a97c429c2f13279
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-03-12 13:38:16 +0100

    Add missing properties from Library model

--- PictureLibrary.Model/Library/Library.cs
+++ PictureLibrary.Model/Library/Library.cs
@@ -5,5 +5,8 @@
         public Guid Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        public List<User>? Owners { get; set; }
+        public List<Tag>? Tags { get; set; }
+        public List<ImageFile>? ImageFiles { get; set; }
     }
 }
