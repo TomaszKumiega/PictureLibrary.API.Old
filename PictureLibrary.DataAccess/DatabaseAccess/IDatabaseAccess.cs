fe3c5b1e3546c02088dc7880be8cb4e0c1076afe
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-03-12 13:41:00 +0100

    Add entity mapping method to the  database access interface

--- PictureLibrary.DataAccess/DatabaseAccess/IDatabaseAccess.cs
+++ PictureLibrary.DataAccess/DatabaseAccess/IDatabaseAccess.cs
@@ -4,6 +4,7 @@
         where TModel : class
     {
         Task<IEnumerable<TModel>> LoadDataAsync(string sql, object parameters);
+        Task<IEnumerable<TModel>> LoadDataAsync<TFirst, TSecond>(string sql, Type[] types, Func<object[], TModel> map, object? parameters = null);
         Task SaveDataAsync<TParameters>(string sql, TParameters parameters) where TParameters : class;
     }
 }
