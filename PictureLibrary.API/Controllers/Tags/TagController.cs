e156af65efeb02bfb48acff34cf892503ead9371
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-03-19 00:14:16 +0100

    Add tag controller

---
+++ PictureLibrary.API/Controllers/Tags/TagController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PictureLibrary.API.Controllers
+{
+    [Route("tags")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public TagController(
+            IMapper mapper,
+            IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+    }
+}
