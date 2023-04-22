a75fcc397d7d6846b0b0912a6e747257b8e38e4a
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-04-22 16:06:48 +0200

    Add UploadSession model

---
+++ PictureLibrary.Model/UploadSession/UploadSession.cs
@@ -0,0 +1,9 @@
+﻿namespace PictureLibrary.Model.UploadSession
+{
+    public class UploadSession
+    {
+        public Guid Id { get; set; }
+        public required string ContentRange { get; set; }
+        public required string FilePath { get; set; }
+    }
+}
