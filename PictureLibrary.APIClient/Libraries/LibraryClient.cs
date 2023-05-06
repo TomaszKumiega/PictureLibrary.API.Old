c6c0a41878543637a3f0574fe9be9d1f3f07da99
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2023-05-06 22:52:01 +0200

    Rename method

--- PictureLibrary.APIClient/Libraries/LibraryClient.cs
+++ PictureLibrary.APIClient/Libraries/LibraryClient.cs
@@ -6,7 +6,7 @@ namespace PictureLibrary.APIClient.Libraries
 {
     public class LibraryClient : ClientBase
     {
-        public async Task<IEnumerable<Library>> GetAllLibraries(AuthorizationData authorizationData, Guid userId)
+        public async Task<IEnumerable<Library>> GetAllLibrariesAsync(AuthorizationData authorizationData, Guid userId)
         {
             var result =  await SendRequestAndDeserializeResponseAsync<GetAllLibrariesResponse>(HttpMethod.Get, $"library/all?userId={userId}", authorizationData: authorizationData);
             return result?.Libraries ?? Enumerable.Empty<Library>();
